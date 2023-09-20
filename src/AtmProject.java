699d8feab539e8dcebff398843d943b07a6a0967
Serkan Karatas <serkan.karatasch@gmail.com>
2023-09-20 12:48:13 +0200

    v2

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -2,6 +2,7 @@ public class AtmProject {
     public static void main(String[] args) {
         System.out.println("Bu bir ATM projesidir");
         //versiyon1
-
+        System.out.println("Java is easy");
+        //versiyon2
     }
 }
