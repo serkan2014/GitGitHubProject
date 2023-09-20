169f1d275cb9e7cf373550a83ca5779d04061476
Serkan Karatas <serkan.karatasch@gmail.com>
2023-09-20 12:50:23 +0200

    v3

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -4,5 +4,7 @@ public class AtmProject {
         //versiyon1
         System.out.println("Java is easy");
         //versiyon2
+        System.out.println("Learn Java Earn Money");
+        //versiyon3
     }
 }
